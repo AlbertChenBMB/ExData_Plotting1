1ee6179352d426fa2b45dc9340644827d894f213
AlbertChen <albirtle93@gmail.com>
2018-10-02 16:23:04 +0900

    fixed the factor to numerice problem
    don't read data as factor

--- plot3.R
+++ plot3.R
@@ -1,4 +1,5 @@
 #plot3
+
 data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
 data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
 data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
@@ -6,5 +7,6 @@ png("plot3.png", width=480, height=480)
 plot(datetime, data$Sub_metering_1, type="l", xlab="", ylab="Energy sub meeting")
 lines(datetime,data$Sub_metering_2,type = "l",col="red")
 lines(datetime,data$Sub_metering_3,type = "l",col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=0.5, col=c("black", "red", "blue"), bty="o")
 dev.off()
 
--- read_data.R
+++ read_data.R
@@ -1,11 +1,11 @@
 #read data
 library(lubridate)
 library(dplyr)
-dataset<-read.csv("household_power_consumption.txt",sep = ";")
+#don't read data sa factor
+dataset<-read.table("household_power_consumption.txt",header = TRUE,sep = ";",stringsAsFactors=FALSE,dec = ".")
 str(dataset)
 head(dataset,2)
 #only take 1/2/2007~2/2/2007
 data<-filter(dataset,Date=="1/2/2007"|Date=="2/2/2007")
-#change the data and time to Date type
-data$Date<-strptime(data$Date,"%d/%m/%Y")
-data$Time<-strptime(data$Time,"%H:%M:%S")
+#change the data and time to Date type and creat datetime
+datetime <- strptime(paste(data$Date, data$Time, sep=" "),"%d/%m/%Y %H:%M:%S") 
